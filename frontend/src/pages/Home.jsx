45350836ef9b3ded93d3a86a3b4cf0bb663f1914
lequythien <zacubj2@gmail.com>
2025-02-14 15:07:43 +0700

    update new 14/02/2025

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Event from '../components/Event'
+import Map from '../components/map'
 
 const Home = () => {
   return (
     <div>
         <Header />
         <Event />
+        <Map />
     </div>
   )
 }
